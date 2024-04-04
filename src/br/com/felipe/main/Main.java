943f653d8dfb6e64c7aa5146f40d645f32182847
Felipe <antoniofelipesilva@live.com>
2024-04-04 18:42:32 -0300

    user structure

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,7 +1,6 @@
 package br.com.felipe.main;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Felipe");
     }
 }
--- src/br/com/felipe/model/User.java
+++ src/br/com/felipe/model/User.java
@@ -1,4 +1,30 @@
 package br.com.felipe.model;
-
 public class User {
+    private String firstName;
+    private int age;
+    private int password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
