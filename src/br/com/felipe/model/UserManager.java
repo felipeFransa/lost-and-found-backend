83aa77d0de3ec592e9ddd93afc2af7df595b06f5
Felipe <antoniofelipesilva@live.com>
2024-04-07 20:38:47 -0300

    user manager characteristics

--- src/br/com/felipe/model/UserManager.java
+++ src/br/com/felipe/model/UserManager.java
@@ -1,4 +1,25 @@
 package br.com.felipe.model;
 
+import java.util.ArrayList;
+
 public class UserManager {
+
+    private ArrayList<String> clients;
+    private String region;
+
+    public ArrayList<String> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<String> clients) {
+        this.clients = clients;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
