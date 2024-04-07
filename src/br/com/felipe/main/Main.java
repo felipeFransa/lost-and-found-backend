bfd2b27117316ebe794d62dfdce274400e1f71a8
Felipe <antoniofelipesilva@live.com>
2024-04-07 20:31:11 -0300

    create user object

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,6 +1,23 @@
 package br.com.felipe.main;
+
+import br.com.felipe.model.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Felipe");
+
+        User newUser = new User();
+
+        newUser.setFirstName("Felipe");
+        newUser.setAge(17);
+        newUser.setPassword(1997);
+
+        var age = newUser.getAge();
+        var name = newUser.getFirstName();
+
+        if (age >= 18){
+            System.out.println("Olá " +name + " Acesso permitido.");
+        }else {
+            System.out.println("Acesso negado.");
+        }
     }
 }
