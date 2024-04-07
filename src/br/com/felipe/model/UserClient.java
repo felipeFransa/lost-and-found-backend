48c48b34a31337a12f803933b32c4f1bd83a9221
Felipe <antoniofelipesilva@live.com>
2024-04-07 20:35:43 -0300

    user client characteristics

--- src/br/com/felipe/model/UserClient.java
+++ src/br/com/felipe/model/UserClient.java
@@ -1,4 +1,23 @@
 package br.com.felipe.model;
 
 public class UserClient {
+
+    private String cityName;
+    private double phoneNumber;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public double getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(double phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
