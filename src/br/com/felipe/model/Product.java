15a1cd1a4d23625e9520b0dfb5c4dfee22aa8e74
felipe <antoniofelipesilva@live.com>
2024-04-17 19:31:03 -0300

    add class product

---
+++ src/br/com/felipe/model/Product.java
@@ -0,0 +1,31 @@
+package br.com.felipe.model;
+
+public class Product {
+    private String nameProduct;
+    private String typeProduct;
+    private double valueProduct;
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getTypeProduct() {
+        return typeProduct;
+    }
+
+    public void setTypeProduct(String typeProduct) {
+        this.typeProduct = typeProduct;
+    }
+
+    public double getValueProduct() {
+        return valueProduct;
+    }
+
+    public void setValueProduct(double valueProduct) {
+        this.valueProduct = valueProduct;
+    }
+}
