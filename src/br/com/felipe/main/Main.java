7459f5a17146595b1d9e7d45a716efa5a867f22a
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-08 16:53:17 -0300

    new structure

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -3,17 +3,23 @@ package br.com.felipe.main;
 import br.com.felipe.model.NewPost;
 import br.com.felipe.model.User;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner reading = new Scanner(System.in);
         User newUser = new User();
         NewPost post = new NewPost();
 
-        post.setAnimalsName("Gato");
+        System.out.println("Digite seu animal:");
+        String animal = reading.nextLine();
+
+        post.setAnimalsName(animal);
         post.setAnimalsType("Felinos");
 
         newUser.setFirstName("Felipe");
-        newUser.setAge(17);
+        newUser.setAge(18);
         newUser.setPassword(1997);
 
         var age = newUser.getAge();
@@ -21,6 +27,7 @@ public class Main {
 
         if (age >= 18){
             System.out.println("Olá " +name + " Acesso permitido.");
+            System.out.println(post.getAnimalsName());
         }else {
             System.out.println("Acesso negado.");
         }
