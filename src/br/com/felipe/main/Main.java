f3fc6310468098e5d4978db941c3008aba4b3bd5
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-11 17:38:18 -0300

    add main welcome

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,31 +1,18 @@
 package br.com.felipe.main;
 
-import br.com.felipe.model.User;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner reading = new Scanner(System.in);
-        System.out.println("Enter your user name:");
-        String userName = reading.nextLine();
-        System.out.println("Enter your age:");
-        int userAge = reading.nextInt();
+        System.out.println("Enter 0 of Client or 1 Manager");
+        Integer opcion = reading.nextInt();
 
-        User newUser = new User();
-        newUser.setFirstName(userName);
-        newUser.setAge(userAge);
-        newUser.setPassword(1997);
 
-        var age = newUser.getAge();
-        var name = newUser.getFirstName();
-
-        if (age >= 18){
-            System.out.println("Hi " +name + " access allowed.");
-
+        if (opcion > 0){
+            System.out.println("Welcome Client");
         }else {
-            System.out.println("Access denied.");
+            System.out.println("Welcome Manager");
         }
     }
 }
