ba318cabbd769625aea1f16b149ade255d82ae4a
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-17 16:19:23 -0300

    new class finds

--- src/br/com/felipe/model/Finds.java
+++ src/br/com/felipe/model/Finds.java
@@ -1,4 +1,31 @@
 package br.com.felipe.model;
 
 public class Finds {
+    private String animalsType;
+    private String animalsName;
+    private String region;
+
+    public String getAnimalsType() {
+        return animalsType;
+    }
+
+    public void setAnimalsType(String animalsType) {
+        this.animalsType = animalsType;
+    }
+
+    public String getAnimalsName() {
+        return animalsName;
+    }
+
+    public void setAnimalsName(String animalsName) {
+        this.animalsName = animalsName;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
