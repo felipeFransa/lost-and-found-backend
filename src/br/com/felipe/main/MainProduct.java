04637bfcca86a8e7dbb51d5dfd3d1f49c02f7f9a
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-18 20:29:02 -0300

    product class

--- src/br/com/felipe/main/MainProduct.java
+++ src/br/com/felipe/main/MainProduct.java
@@ -1,7 +1,13 @@
 package br.com.felipe.main;
 
+import br.com.felipe.model.Product;
+
 public class MainProduct {
     public static void main(String[] args) {
+        Product product = new Product();
 
+        product.setNameProduct("");
+        product.setValueProduct(123);
+        product.setTypeProduct("");
     }
 }
