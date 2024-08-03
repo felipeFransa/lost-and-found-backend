78de2e15c8efd2c9472c6779e0d270c95acf15d5
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-03 15:36:24 -0300

    add condition enter type paln user

--- src/br/com/felipe/main/MainPost.java
+++ src/br/com/felipe/main/MainPost.java
@@ -9,19 +9,30 @@ public class MainPost {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
         NewPost post = new NewPost();
-        System.out.println("enter your animal:");
-        String animal = reading.nextLine();
-        System.out.println("Enter your species:");
-        String species = reading.nextLine();
-        System.out.println("Enter your region");
-        String region = reading.nextLine();
 
-        post.setAnimalsName(animal);
-        post.setAnimalsType(species);
-        post.setRegion(region);
+        System.out.println("enter your type plan");
+        String typePlan = reading.nextLine();
 
-        System.out.println(post.getAnimalsName());
-        System.out.println(post.getAnimalsType());
-        System.out.println(post.getRegion());
+        if (typePlan.equals("plus")){
+            System.out.println("enter your animal:");
+            String animal = reading.nextLine();
+            System.out.println("Enter your species:");
+            String species = reading.nextLine();
+            System.out.println("Enter your region");
+            String region = reading.nextLine();
+
+            post.setAnimalsName(animal);
+            post.setAnimalsType(species);
+            post.setRegion(region);
+
+            System.out.println(post.getAnimalsName());
+            System.out.println(post.getAnimalsType());
+            System.out.println(post.getRegion());
+
+        } else if (typePlan.equals("premie")) {
+            System.out.println("Hello User!");
+        } else {
+            System.out.println("Access denied");
+        }
     }
 }
